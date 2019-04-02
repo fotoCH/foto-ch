81cc4f91374836cc2b738f851554ba7078d9755e
Michael-Schaer <mdschaer@gmail.com>
2019-04-02 15:56:19 +0200

    Create People Migration

--- server/db/migrations/20190402111423_create-people-table.js
+++ server/db/migrations/20190402111423_create-people-table.js
@@ -1,14 +1,27 @@
 
 exports.up = function(knex, Promise) {
-  let createQuery = `CREATE TABLE people(
-    id SERIAL PRIMARY KEY NOT NULL,
-    tel TEXT,
-    email TEXT
-  )`
-  return knex.raw(createQuery)
-};
+  
+    return Promise.all([
+      knex.schema.createTable('people', function (table) {
+        table.increments(),
+        table.integer('GND'),
+        table.text('description'),
+        table.date('birth_date'),
+        table.string('tel'),
+        table.string('email')
+        //table.foreign('people_type_id', 'keyname')
+      }),
+      knex('people').insert(
+        {GND: 101, description: 'some text', birth_date: '2019/11/22', 
+         tel: '0222002002', email: 'test@tail.tom'}),
+      knex('people').insert(
+        {GND: 102, description: 'other text', birth_date: '2019/01/13', 
+         tel: '0311116066', email: 'west@wail.wom'})
+    ])
+}
 
 exports.down = function(knex, Promise) {
-  let dropQuery = `DROP TABLE people`
-  return knex.raw(dropQuery)
-};
+  return Promise.all([
+    knex.schema.dropTableIfExists('people')
+  ])
+}
