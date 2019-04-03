3d4af4258466a334b146e18ccaa953cc8fbcb5d0
raeffu <raeffu@raeffu.ch>
2019-04-03 13:30:15 +0200

    Move graphql routes to separate file

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { Environments, environment } from './configuration';
 import { errorHandler } from './middleware/errors';
 import { timer } from './middleware/timer';
 import { routes } from './routes';
+import { routes as graphqlRoutes } from './routes/graphql';
 
 const app = new Koa();
 
@@ -11,6 +12,7 @@ app
   .use(json({ pretty: environment === Environments.Production }))
   .use(timer)
   .use(errorHandler)
+  .use(graphqlRoutes)
   .use(routes);
 
 export = app;
